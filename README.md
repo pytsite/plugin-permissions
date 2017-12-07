31dfb8d4bccd9819a839954fb35f3f21e09a51a2
Alexander Shepetko <a@shepetko.com>
2017-12-07 12:00:59 +0200

    Bugfix 1.0.1

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 ## Changelog
 
 
+### 1.0.1 (2017-12-07)
+
+Fixed init code.
+
+
 ### 1.0 (2017-11-24)
 
 First release.
--- __init__.py
+++ __init__.py
@@ -1,16 +1,17 @@
 """PytSite Permissions Plugin
 """
-from . import _error as error
-from ._api import define_permission, define_group, get_permission, is_permission_group_defined, \
-    get_permission_groups, get_permissions, is_permission_defined
+from pytsite import plugman as _plugman
+
+# Public API
+if _plugman.is_installed(__name__):
+    from . import _error as error
+    from ._api import define_permission, define_group, get_permission, is_permission_group_defined, \
+        get_permission_groups, get_permissions, is_permission_defined
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
 
-def _init():
+def plugin_load():
     define_group('app', 'app_name')
-
-
-_init()
